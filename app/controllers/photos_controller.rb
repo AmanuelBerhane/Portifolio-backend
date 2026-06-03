906bf7a0fa7d7410e9e67c29e8387bd00d493417
Amanuel Berhane <amanuel.berhane@bitscollege.edu.et>
2026-06-03 12:16:10 +0300

    Image model created

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,51 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, only: %i[ show update destroy ]
+
+  # GET /photos
+  def index
+    @photos = Photo.all
+
+    render json: @photos
+  end
+
+  # GET /photos/1
+  def show
+    render json: @photo
+  end
+
+  # POST /photos
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      render json: @photo, status: :created, location: @photo
+    else
+      render json: @photo.errors, status: :unprocessable_content
+    end
+  end
+
+  # PATCH/PUT /photos/1
+  def update
+    if @photo.update(photo_params)
+      render json: @photo
+    else
+      render json: @photo.errors, status: :unprocessable_content
+    end
+  end
+
+  # DELETE /photos/1
+  def destroy
+    @photo.destroy!
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_photo
+      @photo = Photo.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def photo_params
+      params.expect(photo: [ :image_url ])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_03_061804) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_091453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_03_061804) do
     t.date "date_given"
     t.string "institution"
     t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "image_url"
     t.datetime "updated_at", null: false
   end
 
