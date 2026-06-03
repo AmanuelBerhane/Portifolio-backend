9e5b7679bd4ea218211c2a3271a071fb13ebe2a7
Amanuel Berhane <amanuel.berhane@bitscollege.edu.et>
2026-06-03 09:18:33 +0300

    Skills model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_03_060844) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_061804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -35,6 +35,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_03_060844) do
     t.string "name"
     t.text "tech_stack"
     t.string "timeline"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.string "level"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end
